6390d3d5242b43ddf17b8f16220db1108b2cf3c0
Hatef Palizgar <hatef.palizgar@gamesys.co.uk>
2020-09-19 21:59:12 +0300

    add tests

--- src/test/java/net/mindly/springboot/controller/CryptoControllerTest.java
+++ src/test/java/net/mindly/springboot/controller/CryptoControllerTest.java
@@ -1,7 +1,9 @@
 package net.mindly.springboot.controller;
 
+import net.mindly.springboot.exception.ResourceNotFoundException;
 import net.mindly.springboot.model.Crypto;
 import net.mindly.springboot.repository.CryptoRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -50,38 +53,49 @@ class CryptoControllerTest {
 
 	@Test
 	void testAddCrypto() throws Exception {
-// Setup
 
-// Configure CryptoRepository.save(...).
-		final Crypto crypto = new Crypto("cryptocurrency", new BigDecimal("0.00"), "dateOfPurchase", "walletLocation",
-				new BigDecimal("0.00"), "option");
+		final Crypto crypto = new Crypto(2,"BitCoin", new BigDecimal("50.00"), null, null,
+				null, null);
 		when(mockCryptoRepository.save(any(Crypto.class))).thenReturn(crypto);
 
-// Run the test
-		final MockHttpServletResponse response = mockMvc.perform(post("/api/crypto").content("content")
+		final MockHttpServletResponse response = mockMvc.perform(post("/api/crypto").content("{\n" +
+				"    \"id\": 2,\n" +
+				"    \"cryptocurrency\": \"BitCoin\",\n" +
+				"    \"amount\": 50.00,\n" +
+				"    \"dateOfPurchase\": null,\n" +
+				"    \"walletLocation\": null,\n" +
+				"    \"currentMarketValue\": null,\n" +
+				"    \"option\": null\n" +
+				"}")
 				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
 
-// Verify the results
 		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-		assertThat(response.getContentAsString()).isEqualTo("expectedResponse");
+		assertThat(response.getContentAsString()).contains("cryptocurrency");
 	}
 
 	@Test
 	void testDeleteCrypto() throws Exception {
-// Setup
-
-// Configure CryptoRepository.findById(...).
 		final Optional<Crypto> crypto = Optional.of(new Crypto("cryptocurrency", new BigDecimal("0.00"),
 				"dateOfPurchase", "walletLocation", new BigDecimal("0.00"), "option"));
 		when(mockCryptoRepository.findById(0L)).thenReturn(crypto);
 
-// Run the test
 		final MockHttpServletResponse response = mockMvc
-				.perform(delete("/api/crypto/{id}", 0).accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+				.perform(delete("/api/crypto/{id}", 0)
+						.accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
 
-// Verify the results
 		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-		assertThat(response.getContentAsString()).isEqualTo("expectedResponse");
+		assertThat(response.getContentAsString().contains("deleted"));
 		verify(mockCryptoRepository).delete(any(Crypto.class));
+	}
+
+	@Test
+	void testDeleteCrypto_shouldThrowException() throws Exception {
+		when(mockCryptoRepository.findById(0L)).thenThrow(ResourceNotFoundException.class);
+
+		final MockHttpServletResponse response = mockMvc
+				.perform(delete("/api/crypto/{id}", 0)
+						.accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
 	}
 }
